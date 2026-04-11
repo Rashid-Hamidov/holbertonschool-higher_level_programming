756c2ac034d6a80394dc91e32fd34f9824b6f3b0
ТВОÐRashid-Hamidov <236830707+Rashid-Hamidov@users.noreply.github.com>
2026-04-11 15:15:14 +0000

    a

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 
 add = __import__('add_0').add
 
-a = 1
-b = 2
-
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
