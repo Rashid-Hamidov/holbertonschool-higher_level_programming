7dcc8c0a1b78e2f9627bd2682f0d6ba5ab47a0e4
ТВОЕRashid-Hamidov <236830707+Rashid-Hamidov@users.noreply.github.com>
2026-04-17 18:41:13 +0000

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-
-    for i in range(min(x, len(my_list))):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (TypeError, ValueError):
+        except (IndexError, TypeError, ValueError):
             continue
-
     print()
     return count
